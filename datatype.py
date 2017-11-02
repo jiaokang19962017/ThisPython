b296b27f0eedc56fa9135d134dea03d68657a590
Miracle <811708046@qq.com>
2017-11-02 16:14:38 +0800

    多个变量赋值

--- datatype.py
+++ datatype.py
@@ -1,6 +1,11 @@
-counter=100 # 整型
-miles=1000.0 # 浮点
-name="jiaokang" # 字符串
+counter = 100  # 整型
+miles = 1000.0  # 浮点
+name = "jiaokang"  # 字符串
 print(counter)
 print(miles)
 print(name)
+
+# 多个变量赋值
+a=b=c=1
+a,b,c=1,2,"jiaokang"
+print(a,b,c)
