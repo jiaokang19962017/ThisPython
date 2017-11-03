04f3be17859685212f234baf848450029a1b238c
Miracle <811708046@qq.com>
2017-11-03 13:58:58 +0800

    类的定义和调用

---
+++ object.py
@@ -0,0 +1,13 @@
+# 面向对象
+# 创建一个类
+class MyClass:
+    i=12345
+    def f(self):
+        return 'hello.world'
+
+# 实例化类
+x = MyClass()
+
+# 访问类的属性和方法
+print("myclass类的属性",x.i)
+print("myclass类的方法",x.f())
